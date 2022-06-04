c62f0871e978b9a1f9ccbe70ca9046698c642b8e
alperenyilmaz <alperen@intenseye.com>
2022-06-05 02:38:52 +0300

    Fix tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.calculator import add
+from src.calculator import add, multiply
 
 
 def test_add():
@@ -14,6 +14,6 @@ def test_add():
 def test_multiply():
     value1, value2 = 1, 2
     expected_result = value1 * value2
-    result = add(value1, value2)
+    result = multiply(value1, value2)
 
     assert result == expected_result
